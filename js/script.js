8a0b5e078a30509a12e002764e47ad9d098c71d9
Mostafa <Mostafa_haidare@yahoo.com>
2020-12-12 23:40:53 +0100

    JQuery blev brugt på header "Nykøbing F's hyggeligste bar ". Load hastighed, slideDown og fadeIn blev brugt til Headern i script.js.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    let Load = 2.5;
+
+
+    $(".headerText .divph1").slideDown(Load*400);
+
+
+
+    $(".headerText .divKontakt").delay(Load*400).fadeIn(Load*400);
+
+    /*let headerUnderline1 = gsap.to('.headerUnderline', 1, {delay:Load/1.3, opacity:1});
+
+    let tweenUnderline2 = gsap.to('.headerUnderline', 1, {delay:Load/1.9, marginLeft:0}); */
+
+});
