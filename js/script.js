25d8c52d9bb509b15135ea8f4ce2edd708aa7f66
Mostafa <Mostafa_haidare@yahoo.com>
2020-12-13 14:28:00 +0100

    navigation bar blev stylet, når cursor pointer pager på den.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,7 @@ $(function () {
 
     $(".headerText .divph1").slideDown(Load*400);
 
-
-
     $(".headerText .divKontakt").delay(Load*400).fadeIn(Load*400);
 
-    /*let headerUnderline1 = gsap.to('.headerUnderline', 1, {delay:Load/1.3, opacity:1});
-
-    let tweenUnderline2 = gsap.to('.headerUnderline', 1, {delay:Load/1.9, marginLeft:0}); */
 
 });
